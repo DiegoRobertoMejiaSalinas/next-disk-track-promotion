31f8dd6543e4396640b74f466d95c2e4882173a2
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-09-23 19:11:16 -0500

    fix: Moved Track component

---
+++ src/components/Track/Track.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { FC, useState } from "react";
+import LargeHeading2 from "@/ui/LargeHeading2";
+import styles from "./Track.module.css";
+import { cn } from "@/lib/cn";
+import React from "react";
+import { TRACK_TITLES } from "@/constants/tracks";
+import MusicTrack from "../MusicTrack/MusicTrack";
+import Player from "@/components/Player/Player";
+
+interface TrackProps {}
+
+const Track: FC<TrackProps> = ({}) => {
+  const [currentTrack, setCurrentTrack] = useState(0);
+
+  const onNextTrack = async () => {
+    if (currentTrack == 2) {
+      setCurrentTrack(0);
+    } else {
+      setCurrentTrack(currentTrack + 1);
+    }
+  };
+
+  return (
+    <div className="relative h-screen flex items-center justify-center overflow-x-hidden ">
+      <MusicTrack currentTrack={currentTrack} />
+
+      <div
+        className={cn(
+          "relative container max-w-7xl mx-auto w-full h-full z-50",
+          styles.text_container
+        )}
+      >
+        <div className="h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start ">
+          <LargeHeading2 size={"lg"}>
+            {TRACK_TITLES[currentTrack]}
+          </LargeHeading2>
+        </div>
+
+        <Player
+          onHandleNextTrack={onNextTrack}
+          className="absolute left-1/2 bottom-10 w-60 -translate-x-1/2"
+          currentTrack={currentTrack}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Track;
