c29d3637f775657b8d85e60da0009399ad9668c1
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-09-23 23:58:33 -0500

    feat: Added variant into LargeHeading2

--- src/components/ui/LargeHeading2.tsx
+++ src/components/ui/LargeHeading2.tsx
@@ -10,31 +10,36 @@ interface LargeHeading2Props
   extends HTMLAttributes<HTMLHeadingElement>,
     VariantProps<typeof headingVariants> {}
 
-const headingVariants = cva(
-  "max-w-prose text-slate-700 dark:text-slate-300 font-medium mx-auto text-center",
-  {
-    variants: {
-      size: {
-        default: "text-4xl md:text-5xl lg:text-6xl",
-        sm: "text-2xl md:text-3xl",
-        lg: "text-7xl md:text-8xl lg:text-9xl",
-      },
+const headingVariants = cva("max-w-prose font-medium mx-auto text-center", {
+  variants: {
+    size: {
+      default: "text-4xl md:text-5xl lg:text-6xl",
+      sm: "text-2xl md:text-3xl",
+      lg: "text-7xl md:text-8xl lg:text-9xl",
     },
-    defaultVariants: {
-      size: "default",
+    variant: {
+      default: "text-slate-700 dark:text-slate-300",
+      inverted: "text-slate-300 dark:text-slate-700",
     },
-  }
-);
+  },
+  defaultVariants: {
+    size: "default",
+    variant: "default",
+  },
+});
 
 const LargeHeading2: FC<LargeHeading2Props> = forwardRef<
   HTMLHeadingElement,
   LargeHeading2Props
->(({ className, children, size, ...props }, ref) => {
+>(({ className, children, size, variant, ...props }, ref) => {
   return (
     <h2
       ref={ref}
       {...props}
-      className={cn(headingVariants({ size, className }), openSans.className)}
+      className={cn(
+        headingVariants({ size, variant, className }),
+        openSans.className
+      )}
     >
       <Balancer>{children}</Balancer>
     </h2>
