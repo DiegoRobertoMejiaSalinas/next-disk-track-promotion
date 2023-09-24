844943846f82904fdd1bb4cf160520a1184edc24
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-09-24 00:00:44 -0500

    feat: Added Parallax into page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,38 @@
 import LargeHeading2 from "@/components/ui/LargeHeading2";
 import Track from "@/components/Track/Track";
 import Image from "next/image";
+import ParallaxText from "@/components/ui/ParallaxText";
+import ParallaxWrapper from "@/components/ui/ParallaxWrapper";
+import ParallaxSection from "@/components/ui/ParallaxSection";
+import { Plaster } from "next/font/google";
+
+const plaster = Plaster({ subsets: ["latin"], weight: ["400"] });
 
 export default function Home() {
   return (
     <>
-      <Track />
+      <div className="max-h-screen ">
+        <div className="">
+          <Track />
+        </div>
+
+        <div className="relative h-screen flex flex-col overflow-x-hidden">
+          <ParallaxWrapper>
+            <ParallaxSection baseVelocity={4}>
+              <ParallaxText size={"lg"} className={plaster.className}>
+                JOJI NEW COMING TRACKS
+              </ParallaxText>
+            </ParallaxSection>
+          </ParallaxWrapper>
+          <ParallaxWrapper>
+            <ParallaxSection baseVelocity={-4}>
+              <ParallaxText size={"lg"} className={plaster.className}>
+                JOJI NEW COMING TRACKS
+              </ParallaxText>
+            </ParallaxSection>
+          </ParallaxWrapper>
+        </div>
+      </div>
     </>
   );
 }
