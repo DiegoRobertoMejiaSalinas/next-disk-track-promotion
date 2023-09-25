c0e218c1e3c2b63bdc4de15faddd8fcc7a58e8b1
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-09-24 22:46:25 -0500

    fix: Misclick in ThemeToggle button

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -8,22 +8,20 @@ import { useTheme } from "next-themes";
 interface ThemeToggleProps {}
 
 const ThemeToggle: FC<ThemeToggleProps> = ({}) => {
-  const { setTheme, systemTheme } = useTheme();
+  const { setTheme, systemTheme, theme } = useTheme();
+
+  const changeTheme = () => {
+    if (theme == "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
 
   return (
-    <Button variant="ghost" size="default">
-      <Sun
-        className="rotate-0 scale-100 transition-all hover:text-slate-900 dark:-rotate-90 dark:scale-0 dark:text-slate-400 dark:hover:text-slate-100"
-        onClick={() => {
-          setTheme("dark");
-        }}
-      />
-      <Moon
-        className="absolute rotate-90 scale-0 transition-all hover:text-slate-900 dark:rotate-0 dark:scale-100 dark:text-slate-400 dark:hover:text-slate-100"
-        onClick={() => {
-          setTheme("light");
-        }}
-      />
+    <Button variant="ghost" size="default" onClick={changeTheme}>
+      <Sun className="rotate-0 scale-100 transition-all hover:text-slate-900 dark:-rotate-90 dark:scale-0 dark:text-slate-400 dark:hover:text-slate-100" />
+      <Moon className="absolute rotate-90 scale-0 transition-all hover:text-slate-900 dark:rotate-0 dark:scale-100 dark:text-slate-400 dark:hover:text-slate-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
