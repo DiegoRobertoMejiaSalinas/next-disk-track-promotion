d582e0dd177cd06e31842b503da114c5440dafd7
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-09-25 18:14:01 -0500

    chore: Added variant into Button UI

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -31,6 +31,7 @@ export const buttonVariants = cva(
         ghost:
           "bg-transparent hover:bg-slate-100 dark:hover:bg-slate-800 dark:text-slate-400 data-[state=open]:bg-transparent dark:data-[state=open]:bg-transparent",
         link: "bg-transparent dark:bg-transparent dark:text-slate-100 hover:bg-transparent dark:hover:bg-transparent",
+        clear: "bg-transparent dark:bg-transparent text-slate-100",
       },
       size: {
         default: "h-10 py-2 px-4",
