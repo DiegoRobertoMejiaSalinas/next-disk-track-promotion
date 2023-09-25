633f1d165a99d65e123df1535dd96671678f6dc8
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-09-24 22:18:02 -0500

    feat: Created Paragraph UI Component

---
+++ src/components/ui/Paragraph.tsx
@@ -0,0 +1,48 @@
+import { cn } from "@/lib/cn";
+import { VariantProps, cva } from "class-variance-authority";
+import { Raleway } from "next/font/google";
+import { FC, HTMLAttributes, forwardRef } from "react";
+import { Balancer } from "react-wrap-balancer";
+
+const raleway = Raleway({ subsets: ["latin"] });
+
+interface ParagraphProps
+  extends HTMLAttributes<HTMLParagraphElement>,
+    VariantProps<typeof paragraphVariants> {}
+
+const paragraphVariants = cva(
+  "max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-center",
+  {
+    variants: {
+      size: {
+        default: "text-base sm:text-lg",
+        sm: "text-sm sm:text-base",
+        lg: "text-lg md:text-xl",
+      },
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  }
+);
+
+const Paragraph = forwardRef<HTMLParagraphElement, ParagraphProps>(
+  ({ className, children, size, ...props }, ref) => {
+    return (
+      <p
+        ref={ref}
+        {...props}
+        className={cn(
+          paragraphVariants({ size, className }),
+          raleway.className
+        )}
+      >
+        <Balancer>{children}</Balancer>
+      </p>
+    );
+  }
+);
+
+Paragraph.displayName = "Paragraph";
+
+export default Paragraph;
