8fa76f4f27b3a6024c38d9d49f247246f5c1048b
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-09-25 18:16:52 -0500

    feat: Updated Player and created PlayerExperimental components

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,53 +1,119 @@
 import { TRACKS } from "@/constants/tracks";
 import { cn } from "@/lib/cn";
 import { FC, HTMLAttributes, useEffect, useRef, useState } from "react";
-import { motion } from "framer-motion";
+import { motion, useTime, useTransform } from "framer-motion";
 import styles from "./Player.module.css";
 import { Pause, Play, SkipForward } from "lucide-react";
 import canAutoPlay from "can-autoplay";
+import { useMusicContext } from "@/providers/MusicProviders";
+import { clearInterval } from "timers";
+import { useInterval } from "@/hooks/useInterval";
 
 interface PlayerProps extends HTMLAttributes<HTMLDivElement> {
   currentTrack: number;
-  onHandleNextTrack: () => void;
 }
 
-const Player: FC<PlayerProps> = ({
-  currentTrack,
-  className,
-  onHandleNextTrack,
-  ...props
-}) => {
-  const [isPlaying, setIsPlaying] = useState(false);
+const Player: FC<PlayerProps> = ({ currentTrack, className, ...props }) => {
+  const {
+    isPlaying,
+    setIsPlaying,
+    setIsPaused,
+    setIsSkipped,
+    isPaused,
+    isSkipped,
+    setCurrentTrack,
+  } = useMusicContext();
   const [isRotating, setIsRotating] = useState(false);
+  let fullBarRef = useRef();
+  const [duration, setDuration] = useState(0);
+  const [width, setWidth] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
 
   const musicRef = useRef<HTMLAudioElement>(null);
-  const progressRef = useRef<HTMLInputElement>(null);
+  // const progressRef = useRef<HTMLInputElement>(null);
+
+  useInterval(
+    () => {
+      if (currentTime < duration) {
+        setCurrentTime((t) => t + 1);
+      }
+    },
+    isPlaying ? 1000 : null
+  );
 
   const onPlayPause = () => {
+    // const interval = setInterval(() => {
+    //   setWidth(width + 100);
+    // }, 1000);
+
     if (isPlaying) {
       musicRef.current?.pause();
       setIsPlaying(false);
+      setIsPaused(true);
+      // clearInterval(interval);
     } else {
       musicRef.current?.play();
 
+      setIsPaused(false);
       setIsPlaying(true);
+      // clearInterval(interval);
     }
   };
 
-  const onPlayerNextTrack = () => {
-    setIsRotating(true);
+  const trackToSet = () => {
+    if (currentTrack == TRACKS.length - 1) {
+      return 0;
+    } else {
+      return currentTrack + 1;
+    }
+  };
 
-    onHandleNextTrack();
+  const onPlayerNextTrack = async () => {
+    await setIsRotating(true);
+    await setIsPaused(false);
+    await setIsPlaying(false);
+    await setIsSkipped(true);
 
-    setTimeout(() => {
+    const valueToSet = trackToSet();
+
+    setCurrentTrack(valueToSet);
+
+    if (musicRef?.current) {
+      musicRef.current.pause();
+      musicRef.current.currentTime = 0;
+      musicRef.current.src = TRACKS[valueToSet].musicUrl;
+      // setTimeout(() => {
+      //   console.log(musicRef.current.duration);
+      //   setDuration(musicRef.current.duration);
+      // }, 300);
+      setWidth(0);
+
+      const canBrowserPlayAudio = await canAutoPlay.audio();
+
+      if (!!canBrowserPlayAudio.result) {
+        musicRef.current.play();
+
+        console.log(musicRef.current.played);
+        setIsPaused(false);
+        setIsPlaying(true);
+      }
+
+      // progressRef.current.max = String(musicRef.current.duration);
+      // progressRef.current.value = String(musicRef.current.currentTime);
+    }
+
+    setTimeout(async () => {
       setIsRotating(false);
+      setIsSkipped(false);
     }, 1800);
   };
 
   const handleChangeTrack = async () => {
-    setIsPlaying(false);
+    // setIsPlaying(false);
+    // setIsPaused(false);
+    setIsRotating(true);
 
-    if (progressRef?.current && musicRef?.current) {
+    if (musicRef?.current) {
       musicRef.current.pause();
       musicRef.current.currentTime = 0;
       musicRef.current.src = TRACKS[currentTrack].musicUrl;
@@ -56,17 +122,34 @@ const Player: FC<PlayerProps> = ({
 
       if (!!canBrowserPlayAudio.result) {
         musicRef.current.play();
-        setIsPlaying(true);
-      }
 
-      progressRef.current.max = String(musicRef.current.duration);
-      progressRef.current.value = String(musicRef.current.currentTime);
+        console.log(musicRef.current.played);
+        // setIsPaused(false);
+        // setIsPlaying(true);
+      }
+      setIsRotating(false);
+
+      // progressRef.current.max = String(musicRef.current.duration);
+      // progressRef.current.value = String(musicRef.current.currentTime);
     }
   };
 
+  const setTrackDuration = () => {
+    if (musicRef?.current) {
+      setDuration(musicRef!.current!.duration);
+    }
+    setWidth(0);
+  };
+
   useEffect(() => {
-    handleChangeTrack();
-  }, [currentTrack]);
+    // handleChangeTrack();
+    // setTrackDuration();
+
+    if (!isNaN(musicRef!.current!.duration)) {
+      console.log(musicRef.current!.duration);
+      setDuration(musicRef.current!.duration);
+    }
+  }, [musicRef.current?.duration]);
 
   return (
     <div className={cn("", className)} {...props}>
@@ -78,7 +161,7 @@ const Player: FC<PlayerProps> = ({
       >
         <source src={TRACKS[currentTrack].musicUrl} type="audio/mp3" />
       </audio>
-      <motion.input
+      {/* <motion.input
         layout
         ref={progressRef}
         readOnly
@@ -88,7 +171,22 @@ const Player: FC<PlayerProps> = ({
           "w-full h-1.5 rounded cursor-pointer",
           styles.progress_bar
         )}
-      />
+      /> */}
+
+      <div
+        ref={fullBarRef}
+        className="w-full h-[3px] bg-[#5A526F] rounded-full"
+      ></div>
+
+      <div className="relative h-2 w-full rounded bg-slate-800">
+        <motion.div
+          className="absolute left-0 top-0 h-full bg-dark-gold rounded"
+          style={{
+            width,
+          }}
+          layout
+        ></motion.div>
+      </div>
 
       <div className="control w-full mx-auto grid grid-cols-3 justify-items-center mt-5 ">
         <div></div>
--- src/components/PlayerExperimental/PlayerExperimental.tsx
+++ src/components/PlayerExperimental/PlayerExperimental.tsx
@@ -1,272 +1,157 @@
-import {
-  animate,
-  LayoutGroup,
-  motion,
-  useDragControls,
-  useMotionTemplate,
-  useMotionValue,
-  useTransform,
-} from "framer-motion";
-import { FC, HTMLAttributes, useEffect, useRef, useState } from "react";
-import { Pause, Play } from "lucide-react";
+import { FC, HTMLAttributes, useEffect, useRef } from "react";
+import { Pause, Play, SkipBack, SkipForward } from "lucide-react";
 import { cn } from "@/lib/cn";
 import { useMusicContext } from "@/providers/MusicProviders";
-
-const DURATION = 186;
+import { TRACKS } from "@/constants/tracks";
+import canAutoPlay from "can-autoplay";
+import Button from "../ui/Button";
 
 interface PlayerExperimentalProps extends HTMLAttributes<HTMLDivElement> {}
 
 const PlayerExperimental: FC<PlayerExperimentalProps> = ({ className }) => {
-  const { isPlaying, setIsPlaying, setIsPaused, isPaused } = useMusicContext();
-  let [currentTime, setCurrentTime] = useState(0);
+  const { setIsPlaying, setIsPaused, currentTrack, setCurrentTrack } =
+    useMusicContext();
+  const musicRef = useRef<HTMLAudioElement>(null);
+
+  const onChange = async () => {
+    setIsPlaying(true);
+    setIsPaused(false);
+
+    const canBrowserPlayAudio = await canAutoPlay.audio();
+
+    if (!!canBrowserPlayAudio.result) {
+      await musicRef.current!.load();
+      await musicRef.current!.play();
+      setIsPaused(false);
+      setIsPlaying(true);
+    } else {
+      setIsPaused(true);
+      setIsPlaying(false);
+    }
+  };
+
+  useEffect(() => {
+    onChange();
+  }, [currentTrack]);
+
+  const onPause = () => {
+    setIsPaused(true);
+    setIsPlaying(false);
+    musicRef.current?.pause();
+  };
+
+  const onPlay = () => {
+    setIsPaused(false);
+    setIsPlaying(true);
+    musicRef.current?.play();
+  };
+
+  const onPlayerNextTrack = async () => {
+    const setTrack = () => {
+      if (currentTrack == TRACKS.length - 1) {
+        return 0;
+      } else {
+        return currentTrack + 1;
+      }
+    };
+
+    const track = setTrack();
+    setCurrentTrack(track);
+    musicRef.current!.src = TRACKS[track].musicUrl;
+  };
+
+  const onPlayerBackTrack = async () => {
+    const setTrack = () => {
+      if (currentTrack == 0) {
+        return TRACKS.length - 1;
+      } else {
+        return currentTrack - 1;
+      }
+    };
+    const track = setTrack();
+
+    setCurrentTrack(track);
+
+    musicRef.current!.src = TRACKS[track].musicUrl;
+  };
 
   return (
     <div className={cn(className)}>
-      <ProgressBar
-        playing={isPlaying}
-        currentTime={currentTime}
-        setCurrentTime={setCurrentTime}
-      />
+      <audio
+        onEnded={onPlayerNextTrack}
+        ref={musicRef}
+        controls
+        style={{
+          display: "none",
+        }}
+      >
+        <source src={TRACKS[currentTrack].musicUrl} type="audio/mp3" />
+      </audio>
 
       <PlayerControls
-        playing={isPlaying}
-        onPause={() => {
-          setIsPaused(true);
-          setIsPlaying(false);
-        }}
-        onPlay={() => {
-          setIsPaused(false);
-          setIsPlaying(true);
-        }}
+        onPause={onPause}
+        onPlay={onPlay}
+        onSkipNext={onPlayerNextTrack}
+        onSkipBack={onPlayerBackTrack}
       />
     </div>
   );
 };
 
-function ProgressBar({ playing, currentTime, setCurrentTime }) {
-  let [dragging, setDragging] = useState(false);
-  let constraintsRef = useRef();
-  let fullBarRef = useRef();
-  let scrubberRef = useRef();
-  let scrubberX = useMotionValue(0);
-  let currentTimePrecise = useMotionValue(currentTime);
-  let progressPrecise = useTransform(
-    currentTimePrecise,
-    (v) => (v / DURATION) * 100
-  );
-  let progressPreciseWidth = useMotionTemplate`${progressPrecise}%`;
-  let dragControls = useDragControls();
-
-  let mins = Math.floor(currentTime / 60);
-  let secs = `${currentTime % 60}`.padStart(2, "0");
-  let timecode = `${mins}:${secs}`;
-  let minsRemaining = Math.floor((DURATION - currentTime) / 60);
-  let secsRemaining = `${(DURATION - currentTime) % 60}`.padStart(2, "0");
-  let timecodeRemaining = `${minsRemaining}:${secsRemaining}`;
-  let progress = (currentTime / DURATION) * 100;
-
-  useInterval(
-    () => {
-      if (currentTime < DURATION) {
-        setCurrentTime((t) => t + 1);
-      }
-    },
-    playing ? 1000 : null
-  );
-
-  useInterval(
-    () => {
-      if (currentTime < DURATION) {
-        currentTimePrecise.set(currentTimePrecise.get() + 0.01);
-        let newX = getXFromProgress({
-          containerRef: fullBarRef,
-          progress: currentTimePrecise.get() / DURATION,
-        });
-        scrubberX.set(newX);
-      }
-    },
-    playing ? 10 : null
-  );
-
-  return (
-    <div className="relative w-full mt-[25px]">
-      <div
-        className="relative"
-        onPointerDown={(event) => {
-          let newProgress = getProgressFromX({
-            containerRef: fullBarRef,
-            x: event.clientX,
-          });
-          dragControls.start(event, { snapToCursor: true });
-          setCurrentTime(Math.floor(newProgress * DURATION));
-          currentTimePrecise.set(newProgress * DURATION);
-        }}
-      >
-        <div
-          ref={fullBarRef}
-          className="w-full h-[3px] bg-[#5A526F] rounded-full"
-        ></div>
-        <motion.div
-          layout
-          style={{ width: progressPreciseWidth }}
-          className="absolute top-0"
-        >
-          <div className="absolute inset-0 h-[3px] bg-[#A29CC0] rounded-full"></div>
-        </motion.div>
-        <div
-          className="absolute inset-0 -ml-[15px] -mr-[17px]"
-          ref={constraintsRef}
-        >
-          <motion.button
-            ref={scrubberRef}
-            drag="x"
-            dragConstraints={constraintsRef}
-            dragControls={dragControls}
-            dragElastic={0}
-            dragMomentum={false}
-            style={{ x: scrubberX }}
-            onDrag={() => {
-              let scrubberBounds = scrubberRef.current.getBoundingClientRect();
-              let middleOfScrubber =
-                scrubberBounds.x + scrubberBounds.width / 2;
-              let newProgress = getProgressFromX({
-                containerRef: fullBarRef,
-                x: middleOfScrubber,
-              });
-              setCurrentTime(Math.floor(newProgress * DURATION));
-              currentTimePrecise.set(newProgress * DURATION);
-            }}
-            onDragStart={() => {
-              setDragging(true);
-            }}
-            onPointerDown={() => {
-              setDragging(true);
-            }}
-            onPointerUp={() => {
-              setDragging(false);
-            }}
-            onDragEnd={() => {
-              setDragging(false);
-            }}
-            className="absolute flex items-center justify-center rounded-full cursor-grab active:cursor-grabbing"
-          >
-            <motion.div
-              animate={{ scale: dragging ? 1 : 0.25 }}
-              transition={{ type: "tween", duration: 0.15 }}
-              initial={false}
-              className="w-[33px] h-[33px] bg-[#A29CC0] rounded-full -mt-[15px]"
-            ></motion.div>
-          </motion.button>
-        </div>
-      </div>
-      <div className="flex justify-between mt-[11px]">
-        <motion.p
-          className="absolute left-0 text-[11px] font-medium tracking-wide text-white/20 tabular-nums"
-          animate={{ y: dragging && progress < 12 ? 15 : 0 }}
-        >
-          {timecode}
-        </motion.p>
-
-        <motion.p
-          className="absolute right-0 text-[11px] font-medium tracking-wide text-white/20 tabular-nums"
-          animate={{ y: dragging && progress > 85 ? 15 : 0 }}
-        >
-          -{timecodeRemaining}
-        </motion.p>
-      </div>
-    </div>
-  );
-}
-
-function PlayerControls({ playing, onPause, onPlay }) {
+function PlayerControls({
+  onPause,
+  onPlay,
+  onSkipNext,
+  onSkipBack,
+}: {
+  onPause: () => void;
+  onPlay: () => void;
+  onSkipNext: () => void;
+  onSkipBack: () => void;
+}) {
   const { isPaused, isPlaying } = useMusicContext();
 
   return (
     <div className="mt-6">
-      <div className="flex items-center justify-between px-4">
+      <div className="w-full flex items-center justify-between">
+        <Button
+          onClick={onSkipBack}
+          className="w-12 h-12 p-3 rounded-full"
+          variant={"clear"}
+        >
+          <SkipBack className="w-full h-full" />
+        </Button>
+
         {isPlaying && (
-          <Button onClick={onPause} className="w-12 h-12 p-3">
+          <Button
+            onClick={onPause}
+            className="w-12 h-12 p-3 rounded-full"
+            variant={"clear"}
+          >
             <Pause className="w-full h-full" />
           </Button>
         )}
 
         {isPaused && (
-          <Button onClick={onPlay} className="w-12 h-12 p-3">
+          <Button
+            onClick={onPlay}
+            className="w-12 h-12 p-3 rounded-full"
+            variant={"clear"}
+          >
             <Play className="w-full h-full" />
           </Button>
         )}
+
+        <Button
+          onClick={onSkipNext}
+          className="w-12 h-12 p-3 rounded-full"
+          variant={"clear"}
+        >
+          <SkipForward className="w-full h-full" />
+        </Button>
       </div>
     </div>
   );
-}
-
-function Button({ children, onClick = () => {}, className }) {
-  let [pressing, setPressing] = useState(false);
-
-  return (
-    <motion.button
-      onTapStart={() => {
-        setPressing(true);
-      }}
-      onTap={() => {
-        setPressing(false);
-        onClick();
-      }}
-      initial={false}
-      animate={pressing ? "pressed" : "unpressed"}
-      variants={{
-        pressed: {
-          scale: 0.85,
-          opacity: 0.7,
-        },
-        unpressed: {
-          scale: [null, 0.85, 1],
-          opacity: 1,
-        },
-      }}
-      transition={{
-        type: "spring",
-        duration: 0.3,
-        bounce: 0.5,
-      }}
-      className={`flex items-center justify-center relative text-white rounded-full ${className}`}
-    >
-      {children}
-    </motion.button>
-  );
-}
-
-function getProgressFromX({ x, containerRef }) {
-  let bounds = containerRef.current.getBoundingClientRect();
-  let progress = (x - bounds.x) / bounds.width;
-
-  return clamp(progress, 0, 1);
-}
-
-function getXFromProgress({ progress, containerRef }) {
-  let bounds = containerRef.current.getBoundingClientRect();
-
-  return progress * bounds.width;
-}
-
-function clamp(number, min, max) {
-  return Math.max(min, Math.min(number, max));
-}
-
-function useInterval(callback, delay) {
-  const intervalRef = useRef(null);
-  const savedCallback = useRef(callback);
-  useEffect(() => {
-    savedCallback.current = callback;
-  }, [callback]);
-  useEffect(() => {
-    const tick = () => savedCallback.current();
-    if (typeof delay === "number") {
-      intervalRef.current = window.setInterval(tick, delay);
-      return () => window.clearInterval(intervalRef.current);
-    }
-  }, [delay]);
-  return intervalRef;
 }
 
 export default PlayerExperimental;
