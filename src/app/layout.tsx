b888ddc1098e07ed74f3279368f1421d41515764
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-09-25 18:16:23 -0500

    chore: Added Toaster into Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Providers from "@/providers/Providers";
 import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,8 @@ export default function RootLayout({
       <body className={inter.className}>
         <Providers>
           {children}
+
+          <Toaster position="bottom-right" />
 
           <Navbar />
         </Providers>
