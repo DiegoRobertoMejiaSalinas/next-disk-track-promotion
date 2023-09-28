8b92546e5049369d96e6ba57a2a8ed08b7e5dc38
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-09-27 20:20:04 -0500

    feat: Added og images and descriptions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,40 @@ export const metadata: Metadata = {
   title: "JOJI | Unofficial Website",
   description:
     "This isn't an official Joji website, this is a personal project made by Diego Mejia, a big Joji fan.",
+  applicationName: "JOJI | Unofficial Website",
+  authors: {
+    name: "Diego Mejia",
+    url: "https://www.linkedin.com/in/diego-roberto-mejia-salinas-73a931198/",
+  },
+  creator: "Diego Mejia",
+  keywords: "JOJI music Diego Mejia Tracks The Smithereens",
+  twitter: {
+    title: "JOJI | Unofficial Website",
+    description: "https://next-track-promotion.hedgehog-testing.xyz/",
+  },
+
+  openGraph: {
+    type: "music.album",
+    countryName: "USA",
+    siteName: "JOJI | Unofficial Website",
+    title: "JOJI | Unofficial Website",
+    url: "https://next-track-promotion.hedgehog-testing.xyz/",
+    description:
+      "This isn't an official Joji website, this is a personal project made by Diego Mejia, a big Joji fan.",
+    musicians: "JOJI",
+    songs: [
+      "Glimpse Of Us",
+      "Feeling Like The End",
+      "Die For You",
+      "Before the Day is Over",
+      "Dissolve",
+      "Night Rider",
+      "BlahBlahBlah Demo",
+      "Yukon (Interlude)",
+      "1AM Freestyle",
+    ],
+  },
+
   alternates: {
     canonical: "https://next-track-promotion.hedgehog-testing.xyz/",
   },
