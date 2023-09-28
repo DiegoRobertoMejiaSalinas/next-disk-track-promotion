929e16acfaed09b0770cb4224338ec484afbf8c0
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-09-27 23:23:02 -0500

    feat: Updated tracks images

--- src/constants/tracks.ts
+++ src/constants/tracks.ts
@@ -9,55 +9,55 @@ interface ITrackItem {
 export const TRACKS: ITrackItem[] = [
   {
     imageUrl:
-      "https://res.cloudinary.com/purplesoda/image/upload/v1695479593/Test%20Projects/Joji%20Album%20Collection/pexels-isaac-weatherly-2131647_fgj8gs.webp",
+      "https://res.cloudinary.com/purplesoda/image/upload/v1695874798/Test%20Projects/Joji%20Album%20Collection/person-modified_1_gbgmxt.webp",
     musicUrl: "/tracks/glimpse_of_us.mp3",
     title: "Glimpse of Us",
   },
   {
     imageUrl:
-      "https://res.cloudinary.com/purplesoda/image/upload/v1695479593/Test%20Projects/Joji%20Album%20Collection/pexels-isaac-weatherly-2131647_fgj8gs.webp",
+      "https://res.cloudinary.com/purplesoda/image/upload/v1695873754/Test%20Projects/Joji%20Album%20Collection/pexels-tiana-614484-modified_wreh6a.webp",
     musicUrl: "/tracks/feeling_like_the_end.mp3",
     title: "Feeling Like the End",
   },
   {
     imageUrl:
-      "https://res.cloudinary.com/purplesoda/image/upload/v1695479593/Test%20Projects/Joji%20Album%20Collection/pexels-isaac-weatherly-2131647_fgj8gs.webp",
+      "https://res.cloudinary.com/purplesoda/image/upload/v1695514880/Test%20Projects/Joji%20Album%20Collection/alberto-restifo-cFplR9ZGnAk-unsplash_1_-modified_gpaazc.webp",
     musicUrl: "/tracks/die_for_you.mp3",
     title: "Die for You",
   },
   {
     imageUrl:
-      "https://res.cloudinary.com/purplesoda/image/upload/v1695479593/Test%20Projects/Joji%20Album%20Collection/pexels-isaac-weatherly-2131647_fgj8gs.webp",
+      "https://res.cloudinary.com/purplesoda/image/upload/v1695873754/Test%20Projects/Joji%20Album%20Collection/pexels-tobias-bj%C3%B8rkli-1559817-modified_hwrd31.webp",
     musicUrl: "/tracks/before_the_day_is_over.mp3",
     title: "Before the Day is Over",
   },
   {
     imageUrl:
-      "https://res.cloudinary.com/purplesoda/image/upload/v1695479593/Test%20Projects/Joji%20Album%20Collection/pexels-isaac-weatherly-2131647_fgj8gs.webp",
+      "https://res.cloudinary.com/purplesoda/image/upload/v1695874696/Test%20Projects/Joji%20Album%20Collection/pexels-s-migaj-1237528_1_-modified_kss1e9.webp",
     musicUrl: "/tracks/dissolve.mp3",
     title: "Dissolve",
   },
   {
     imageUrl:
-      "https://res.cloudinary.com/purplesoda/image/upload/v1695479593/Test%20Projects/Joji%20Album%20Collection/pexels-isaac-weatherly-2131647_fgj8gs.webp",
+      "https://res.cloudinary.com/purplesoda/image/upload/v1695873754/Test%20Projects/Joji%20Album%20Collection/pexels-furknsaglam-3109671_1_-modified_zzzpxv.webp",
     musicUrl: "/tracks/night_rider.mp3",
     title: "Night Rider",
   },
   {
     imageUrl:
-      "https://res.cloudinary.com/purplesoda/image/upload/v1695479593/Test%20Projects/Joji%20Album%20Collection/pexels-isaac-weatherly-2131647_fgj8gs.webp",
+      "https://res.cloudinary.com/purplesoda/image/upload/v1695873758/Test%20Projects/Joji%20Album%20Collection/pexels-benjamin-suter-3733269-modified_mahngy.webp",
     musicUrl: "/tracks/blah_blah_blah_demo.mp3",
     title: "BlahBlahBlah Demo",
   },
   {
     imageUrl:
-      "https://res.cloudinary.com/purplesoda/image/upload/v1695479593/Test%20Projects/Joji%20Album%20Collection/pexels-isaac-weatherly-2131647_fgj8gs.webp",
+      "https://res.cloudinary.com/purplesoda/image/upload/v1695873754/Test%20Projects/Joji%20Album%20Collection/pexels-ali-kazal-15250205-modified_sveoyh.webp",
     musicUrl: "/tracks/yukon_interlude.mp3",
     title: "Yukon (Interlude)",
   },
   {
     imageUrl:
-      "https://res.cloudinary.com/purplesoda/image/upload/v1695479593/Test%20Projects/Joji%20Album%20Collection/pexels-isaac-weatherly-2131647_fgj8gs.webp",
+      "https://res.cloudinary.com/purplesoda/image/upload/v1695874411/Test%20Projects/Joji%20Album%20Collection/pexels-zeeshaan-shabbir-5417109-modified_shxfx0.webp",
     musicUrl: "/tracks/1am_freestyle.mp3",
     title: "1AM Freestyle",
   },
