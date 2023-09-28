d36c45f049bfcc6de8fc3d3ca4020fb3d4903dee
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-09-27 22:27:42 -0500

    feat: Updated tracks

--- src/constants/tracks.ts
+++ src/constants/tracks.ts
@@ -1,13 +1,4 @@
-export const TRACK_TITLES = [
-  "Slow dancing in the dark",
-  "Nitrous",
-  "1AM Freestyle",
-];
-export const TRACK_MP3_URL = [
-  "/tracks/slow_dancing.mp3",
-  "/tracks/nitrous.mp3",
-  "/tracks/1am_freestyle.mp3",
-];
+
 
 interface ITrackItem {
   title: string;
@@ -19,25 +10,56 @@ export const TRACKS: ITrackItem[] = [
   {
     imageUrl:
       "https://res.cloudinary.com/purplesoda/image/upload/v1695479593/Test%20Projects/Joji%20Album%20Collection/pexels-isaac-weatherly-2131647_fgj8gs.webp",
-    musicUrl: "/tracks/slow_dancing.mp3",
-    title: "Slow Dancing in the dark",
+    musicUrl: "/tracks/glimpse_of_us.mp3",
+    title: "Glimpse of Us",
   },
   {
     imageUrl:
-      "https://res.cloudinary.com/purplesoda/image/upload/v1695479594/Test%20Projects/Joji%20Album%20Collection/ilona-bellotto-rbPplWrpgcU-unsplash_djyw93.webp",
-    musicUrl: "/tracks/nitrous.mp3",
-    title: "Nitrous",
+      "https://res.cloudinary.com/purplesoda/image/upload/v1695479593/Test%20Projects/Joji%20Album%20Collection/pexels-isaac-weatherly-2131647_fgj8gs.webp",
+    musicUrl: "/tracks/feeling_like_the_end.mp3",
+    title: "Feeling Like the End",
   },
   {
     imageUrl:
-      "https://res.cloudinary.com/purplesoda/image/upload/v1695479594/Test%20Projects/Joji%20Album%20Collection/pexels-alex-umbelino-18280489_f5kdqu.webp",
+      "https://res.cloudinary.com/purplesoda/image/upload/v1695479593/Test%20Projects/Joji%20Album%20Collection/pexels-isaac-weatherly-2131647_fgj8gs.webp",
+    musicUrl: "/tracks/die_for_you.mp3",
+    title: "Die for You",
+  },
+  {
+    imageUrl:
+      "https://res.cloudinary.com/purplesoda/image/upload/v1695479593/Test%20Projects/Joji%20Album%20Collection/pexels-isaac-weatherly-2131647_fgj8gs.webp",
+    musicUrl: "/tracks/before_the_day_is_over.mp3",
+    title: "Before the Day is Over",
+  },
+  {
+    imageUrl:
+      "https://res.cloudinary.com/purplesoda/image/upload/v1695479593/Test%20Projects/Joji%20Album%20Collection/pexels-isaac-weatherly-2131647_fgj8gs.webp",
+    musicUrl: "/tracks/dissolve.mp3",
+    title: "Dissolve",
+  },
+  {
+    imageUrl:
+      "https://res.cloudinary.com/purplesoda/image/upload/v1695479593/Test%20Projects/Joji%20Album%20Collection/pexels-isaac-weatherly-2131647_fgj8gs.webp",
+    musicUrl: "/tracks/night_rider.mp3",
+    title: "Night Rider",
+  },
+  {
+    imageUrl:
+      "https://res.cloudinary.com/purplesoda/image/upload/v1695479593/Test%20Projects/Joji%20Album%20Collection/pexels-isaac-weatherly-2131647_fgj8gs.webp",
+    musicUrl: "/tracks/blah_blah_blah_demo.mp3",
+    title: "BlahBlahBlah Demo",
+  },
+  {
+    imageUrl:
+      "https://res.cloudinary.com/purplesoda/image/upload/v1695479593/Test%20Projects/Joji%20Album%20Collection/pexels-isaac-weatherly-2131647_fgj8gs.webp",
+    musicUrl: "/tracks/yukon_interlude.mp3",
+    title: "Yukon (Interlude)",
+  },
+  {
+    imageUrl:
+      "https://res.cloudinary.com/purplesoda/image/upload/v1695479593/Test%20Projects/Joji%20Album%20Collection/pexels-isaac-weatherly-2131647_fgj8gs.webp",
     musicUrl: "/tracks/1am_freestyle.mp3",
     title: "1AM Freestyle",
   },
-  {
-    imageUrl:
-      "https://res.cloudinary.com/purplesoda/image/upload/v1695514880/Test%20Projects/Joji%20Album%20Collection/alberto-restifo-cFplR9ZGnAk-unsplash_1_-modified_gpaazc.webp",
-    musicUrl: "/tracks/blahblahblah.mp3",
-    title: "BlahBlahBlah Demo",
-  },
+
 ];
