8c8968a1f0656ac0b81721d0da0ceb2bc5ec8cea
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-12-24 17:40:14 -0500

    feat: Added Google Tag Manager script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,6 +62,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WS9FF996');
+        `}
+      </Script>
+
       <body className={inter.className}>
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-3H66C7Z3FH" />
         <Script id="google-analytics">
@@ -84,6 +94,12 @@ export default function RootLayout({
 
         {/* Allow for more height on mobile devices */}
         <div className="h-40 md:hidden"></div>
+
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WS9FF996" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
       </body>
     </html>
   );
