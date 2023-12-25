19be0550acf874c8408cbe2f136087591ef626df
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-12-25 11:08:23 -0500

    chore: Minimal changes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,12 @@
 import { MetadataRoute } from "next";
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap>{
-    return [
-        {
-            url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
-            lastModified: new Date(),
-            // priority: 
-            // changeFrequency: "monthly"
-        }
-    ]
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
+      lastModified: new Date(),
+      priority: 0.9,
+      changeFrequency: "yearly",
+    },
+  ];
 }
